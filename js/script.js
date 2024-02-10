4163da13905200504bdccaed8c7588b169dd72d7
lvivduncan <lvivduncan@gmail.com>
2024-02-10 20:40:12 +0200

    mobile start

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 "strict mode"
+
+const menuButton = document.getElementById("menu-button")
+const menu = document.getElementById("menu")
+
+menuButton.addEventListener("click", () => {
+	menu.classList.toggle("active")
+	menuButton.classList.toggle("active")
+})
