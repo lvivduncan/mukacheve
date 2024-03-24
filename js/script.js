69f749dc30f720109f9ff551e0f0f44051223b2c
lvivduncan <lvivduncan@gmail.com>
2024-03-24 13:36:57 +0100

    masonry

--- js/script.js
+++ js/script.js
@@ -78,3 +78,37 @@ document.body.addEventListener('click', function(event){
 		addNews.className = ''	
 	}
 })
+
+// media home page
+{
+    const headers = document.querySelectorAll('.masonry-header li')
+    const mains = document.querySelectorAll('.masonry article')
+
+    for(let i = 0; i < headers.length; i++){
+
+        headers[i].addEventListener('click', function(){
+
+            if(this.dataset.header === 'ALL') {
+
+                for(let k = 0; k < mains.length; k++) {
+
+                    mains[k].className = 'show'
+                }
+            } else {
+
+                for(let k = 0; k < mains.length; k++){
+
+                    if(mains[k].dataset.main.includes(headers[i].dataset.header)){
+
+                        mains[k].className = 'show'
+                    } else {
+                        
+                        mains[k].className = 'hidden'
+                    }
+                }            
+            }
+
+
+        })
+    }
+}
